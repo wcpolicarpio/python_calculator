ac5d82af0233a639163bc77f4c82d2bb06bc8906
wcpolicarpio <walter.policarpio@morgansolar.com>
2017-02-17 15:20:52 -0500

    Correct

--- app/tests/test_calculator.py
+++ app/tests/test_calculator.py
@@ -11,7 +11,7 @@ class TddInPythonExample(unittest.TestCase):
  
     def test_calculator_add_method_returns_correct_result(self):
         calc = Calculator()
-        result = calc.add(2,3)
+        result = calc.add(2,2)
         self.assertEqual(4, result)
  
  
